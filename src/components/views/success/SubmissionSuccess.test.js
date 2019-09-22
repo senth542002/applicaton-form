99729bc127202909bb93a3f86b09a9dfd3406549
senth542002 <senth542002@gmail.com>
2019-09-23 00:17:04 +0530

    Added new test

--- src/components/views/success/SubmissionSuccess.test.js
+++ src/components/views/success/SubmissionSuccess.test.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { mount } from 'enzyme'
 import SubmissionSuccess from './SubmissionSuccess'
 //import renderer from 'react-test-renderer'
-//import mockAxios from 'jest-mock-axios';
+//import mockAxios from 'jest-mock-axios'
+
 
 describe('Submission Success Component', () => {
 
@@ -27,6 +28,20 @@ describe('Submission Success Component', () => {
         console.log('Application Number: '+wrapper.instance().state.applicationNumber)
         expect(wrapper.exists('.applicationNumber')).toEqual(true)
         expect(wrapper.instance().state.applicationNumber).toEqual(114)
+
+        //expect(text).toEqual('Student Name: ');
+      })
+
+      it('Verify PDF Generation', () => {
+
+        const wrapper = mount(<SubmissionSuccess  {...props} />)
+        console.log('anchor value: '+wrapper.find('a').at(0).props().href)
+        const applicationLink = wrapper.find('a').simulate('click');
+
+        setTimeout(function() {
+            expect(wrapper.instance().state.active).toEqual(false)
+        }.bind(this), 20000)
+
 
         //expect(text).toEqual('Student Name: ');
       })
